4060fed23e9b2b3242abedd5ca49962a86276323
Jean Pita <re44e@outlook.com.br>
2021-01-28 15:50:42 -0300

    build: add primary configuration

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:alpine
+
+WORKDIR /usr/app
+
+COPY ["package.json", "yarn.lock", "./"]
+
+RUN yarn
+
+COPY . .
+
+EXPOSE 5000
+
+CMD [ "yarn", "dev" ]
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  // Stop running tests after `n` failures
+  bail: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ["**/__tests__/**/*spec.ts?(x)"],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "__tests__/coverage",
+  preset: "ts-jest",
+};
