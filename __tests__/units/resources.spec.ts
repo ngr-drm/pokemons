e5539be9a43bed05a0c7a31a07f95df1455140e7
Jean Pita <re44e@outlook.com.br>
2021-01-29 22:54:34 -0300

    test: update unit tests

--- __tests__/units/resources.spec.ts
+++ __tests__/units/resources.spec.ts
@@ -1,5 +1,5 @@
 import { IPlayGame } from '../../src/core/business/value-object/pokemons-dto'
-import {jest} from '@jest/globals'
+import { jest } from '@jest/globals'
 
 describe('Validations', () => {
   it('will check if the Pokemon type is valid', async () => {
@@ -18,18 +18,18 @@ describe('Validations', () => {
   });
 
   it('will test the battle algorithm for pokemons of equal levels.', async () => {
-    
 
-    const algorithm = function (PokemonA: IPlayGame, PokemonB: IPlayGame){
+
+    const algorithm = function (PokemonA: IPlayGame, PokemonB: IPlayGame) {
 
       const levelA = PokemonA.level;
       const levelB = PokemonB.level;
-  
-      if (levelA === levelB){
-  
+
+      if (levelA === levelB) {
+
         const reference = Math.floor((Math.random() * 100) + 0);
-  
-        if (reference >= 0 && reference <= 49){
+
+        if (reference >= 0 && reference <= 49) {
           const result = {
             winner: PokemonA.id,
             WinnerLevel: levelA + 1,
@@ -39,18 +39,18 @@ describe('Validations', () => {
           console.log(result);
           return result;
         }
-  
-        if (reference >= 50 && reference <= 100){
+
+        if (reference >= 50 && reference <= 100) {
           const result = {
             winner: PokemonB.id,
             WinnerLevel: levelB + 1,
             loser: PokemonA.id,
             loserLevel: levelA - 1
           }
-          console.log(result); 
+          console.log(result);
           return result;
         }
-       
+
       }
     }
 
@@ -63,17 +63,16 @@ describe('Validations', () => {
 
   it('will test the battle algorithm for pokemons of different levels.', async () => {
 
-    const algorithm = function (PokemonA: IPlayGame, PokemonB: IPlayGame){
+    const algorithm = function (PokemonA: IPlayGame, PokemonB: IPlayGame) {
 
       const levelA = PokemonA.level;
       const levelB = PokemonB.level;
-  
-      if (levelA > levelB){
-  
+
+      if (levelA > levelB) {
+
         const reference = Math.floor((Math.random() * 100) + 0);
-        console.log(reference);
-  
-        if (reference >= 34 && reference <= 100){
+
+        if (reference >= 34 && reference <= 100) {
           const result = {
             winner: PokemonA.id,
             WinnerLevel: levelA + 1,
@@ -83,48 +82,47 @@ describe('Validations', () => {
           console.log(result);
           return result;
         }
-  
-        if (reference >= 0 && reference <= 33){
+
+        if (reference >= 0 && reference <= 33) {
           const result = {
             winner: PokemonB.id,
             WinnerLevel: levelB + 1,
             loser: PokemonA.id,
             loserLevel: levelA - 1
           }
-          console.log(result); 
+          console.log(result);
           return result;
         }
-       
+
+      } else {
+
+        const reference = Math.floor((Math.random() * 100) + 0);
+
+        if (reference >= 34 && reference <= 100) {
+          const result = {
+            winner: PokemonB.id,
+            WinnerLevel: levelB + 1,
+            loser: PokemonA.id,
+            loserLevel: levelA - 1
+          }
+          console.log(result);
+          return result;
+        }
+
+        if (reference >= 0 && reference <= 33) {
+          const result = {
+            winner: PokemonA.id,
+            WinnerLevel: levelA + 1,
+            loser: PokemonB.id,
+            loserLevel: levelB - 1
+          }
+          console.log(result);
+          return result;
+        }
+
       }
 
-      if (levelA < levelB){
-  
-        const reference = Math.floor((Math.random() * 100) + 0);
-        console.log(reference);
-  
-        if (reference >= 34 && reference <= 100){
-          const result = {
-            winner: PokemonB.id,
-            WinnerLevel: levelB + 1,
-            loser: PokemonA.id,
-            loserLevel: levelA - 1
-          }
-          console.log(result);
-          return result;
-        }
-  
-        if (reference >= 0 && reference <= 33){
-          const result = {
-            winner: PokemonA.id,
-            WinnerLevel: levelA + 1,
-            loser: PokemonB.id,
-            loserLevel: levelB - 1
-          }
-          console.log(result); 
-          return result;
-        }
-       
-      }
+
     }
 
     const PokemonA: IPlayGame = { id: 'A', level: 2 }
@@ -133,5 +131,5 @@ describe('Validations', () => {
     myMockFn(PokemonA, PokemonB);
 
   });
-  
+
 });
